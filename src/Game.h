fae1f24848909439b5c6f2b7cbf21178fc44fa3d
siddhant <siddhant2232sharma2232@gmail.com>
2025-08-13 17:37:15 +0530

    fixed

--- src/Game.h
+++ src/Game.h
@@ -13,9 +13,17 @@ class Game
     sf::RenderWindow* window;
     sf::Event ev;
     sf::VideoMode videmode;
+    sf::RectangleShape object;
+    sf::FloatRect bound;
+    sf::Vector2u window_size;
+    sf::Vector2f pos;
+    sf::Vector2f velocity = {2.0f , 0.0f};
 
     void initVariable();
     void initWindow();
+    void initobject();
+
+
 
     public:
     //constructors/destrustors
@@ -24,7 +32,6 @@ class Game
     const bool isWindowOpen() const;
 
     //Methods
-    void SpriteRunner();
     void pollEvents();
     void update();
     void render();
